31d529e3f51b0afaf6ee6af9f29322c34f7c3193
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-01-04 22:16:23 +0900

    Tweak

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import { render } from 'react-dom';
 import React, { Component, PropTypes } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import {
+  Avatar, Card, CardActions,
+  CardHeader, CardMedia, CardTitle,
+  RaisedButton, FlatButton, CardText
+} from 'material-ui';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <Card>
+        <CardHeader
+          title="Title"
+          subtitle="Subtitle"
+          avatar={<Avatar style={{color: 'red'}}>A</Avatar>}/>
+        <CardHeader
+          title="Demo Url Based Avatar"
+          subtitle="Subtitle"
+          avatar="http://lorempixel.com/100/100/nature/"/>
+        <CardMedia overlay={<CardTitle title="Title" subtitle="Subtitle"/>}>
+          <img src="http://lorempixel.com/600/337/nature/"/>
+        </CardMedia>
+        <CardTitle title="Title" subtitle="Subtitle"/>
+        <CardActions>
+          <RaisedButton primary={true} label="Action1"/>
+          <FlatButton label="Action2"/>
+        </CardActions>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+      </Card>
     );
   }
 }
